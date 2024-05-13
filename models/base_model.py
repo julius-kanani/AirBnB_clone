abdfabbeaccc3bca10d62fc964952407a562bdb2
julius-kanani <juliusm.kanani@gmail.com>
2024-05-13 16:48:33 +0000

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ base_module for the BaseModel class."""
-import uuid
 from datetime import datetime
+import models
+import uuid
+
 
 
 class BaseModel:
@@ -48,6 +50,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -64,6 +67,7 @@ class BaseModel:
         Updates the instance's updated_at attribute to the current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" file_storage module that supplies the FileStorage class. """
+import importlib
+import json
+from models.base_model import BaseModel
+
+
+classes = {"BaseModel": BaseModel}
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to
+    instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists).
+
+        If the JSON file exists, loads the objects from the file and adds
+        them to __objects dictionary.
+        Each object is instantiated based on its class name retrieved from
+        the JSON file and added to the dictionary.
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                loaded_objects = json.load(file)
+                for key, obj_dict in loaded_objects.items():
+                    class_name, obj_id = key.split('.')
+                    cls = classes[class_name]
+                    obj = cls(**obj_dict)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
